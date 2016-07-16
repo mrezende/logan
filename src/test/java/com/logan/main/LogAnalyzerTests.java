df6b407f3a2a30ee506e5c0865e3ee67ccb74af4
Marcelo Martins <rezende.martins@gmail.com>
2016-07-15 23:27:25 -0300

    testes parametrizados

--- src/test/java/com/logan/main/LogAnalyzerTests.java
+++ src/test/java/com/logan/main/LogAnalyzerTests.java
@@ -1,8 +1,12 @@
 package com.logan.main;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class LogAnalyzerTests {
 	@Test
 	public void isValidLogFilename_BadExtension_ReturnsFalse() {
@@ -12,16 +16,11 @@ public class LogAnalyzerTests {
 	}
 
 	@Test
-	public void isValidLogFilename_GoodExtensionLowerCase_ReturnsTrue() {
+	@Parameters({"filewithgoodextension.SLF", "filewithgoodextension.slf"})
+	public void isValidLogFilename_ValidExtensions_ReturnsTrue(String filename) {
 		LogAnalyzer logAnalyzer = new LogAnalyzer();
-		boolean isValid = logAnalyzer.isValidLogFilename("filewithgoodextension.slf");
-		Assert.assertTrue(isValid);
-	}
+		boolean isValid = logAnalyzer.isValidLogFilename(filename);
 
-	@Test
-	public void isValidLogFilename_GoodExtensionUpperCase_ReturnsTrue() {
-		LogAnalyzer logAnalyzer = new LogAnalyzer();
-		boolean isValid = logAnalyzer.isValidLogFilename("filewithgoodextension.SLF");
 		Assert.assertTrue(isValid);
 	}
 
