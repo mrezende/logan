383c1bf0e91acdddb033e2341fc4f6fe6d9b7b18
Marcelo Martins <rezende.martins@gmail.com>
2016-07-23 16:13:45 -0300

    system state changes

--- src/main/java/com/logan/main/LogAnalyzer.java
+++ src/main/java/com/logan/main/LogAnalyzer.java
@@ -2,8 +2,11 @@ package com.logan.main;
 
 public class LogAnalyzer {
 
+	private boolean lastFilenameValid;
+
 	public boolean isValidLogFilename(String filename) {
 
+		this.lastFilenameValid = false;
 		if(filename == null || filename.trim() == "") {
 			throw new IllegalArgumentException("filename has to be provided");
 		}
@@ -11,7 +14,17 @@ public class LogAnalyzer {
 		if (!filename.toUpperCase().endsWith(".SLF")) {
 			return false;
 		}
+
+		this.lastFilenameValid = true;
 		return true;
+	}
+
+	public boolean isLastFilenameValid() {
+		return lastFilenameValid;
+	}
+
+	public void setLastFilenameValid(boolean lastFilenameValid) {
+		this.lastFilenameValid = lastFilenameValid;
 	}
 
 }
--- src/test/java/com/logan/main/LogAnalyzerTests.java
+++ src/test/java/com/logan/main/LogAnalyzerTests.java
@@ -3,15 +3,19 @@ package com.logan.main;
 import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JUnitParamsRunner.class)
 public class LogAnalyzerTests {
+
+	private static LogAnalyzer makeAnalyzer() {
+		return new LogAnalyzer();
+	}
+
 	@Test
 	public void isValidLogFilename_BadExtension_ReturnsFalse() {
-		LogAnalyzer logAnalyzer = new LogAnalyzer();
+		LogAnalyzer logAnalyzer = makeAnalyzer();
 		boolean isValid = logAnalyzer.isValidLogFilename("filewithbadextension.foo");
 		Assert.assertFalse(isValid);
 	}
@@ -19,7 +23,7 @@ public class LogAnalyzerTests {
 	@Test
 	@Parameters({"filewithgoodextension.SLF", "filewithgoodextension.slf"})
 	public void isValidLogFilename_ValidExtensions_ReturnsTrue(String filename) {
-		LogAnalyzer logAnalyzer = new LogAnalyzer();
+		LogAnalyzer logAnalyzer = makeAnalyzer();
 		boolean isValid = logAnalyzer.isValidLogFilename(filename);
 
 		Assert.assertTrue(isValid);
@@ -27,9 +31,19 @@ public class LogAnalyzerTests {
 
 	@Test(expected = IllegalArgumentException.class)
 	public void isValidLogFilename_EmptyFilename_ThrowsException() {
-		LogAnalyzer logAnalyzer = new LogAnalyzer();
+		LogAnalyzer logAnalyzer = makeAnalyzer();
 		logAnalyzer.isValidLogFilename("");
 
+
+	}
+
+	@Test
+	@Parameters({ "badname.foo, false", "goodname.slf, true"})
+	public void isValidLogFilename_WhenCalled_ChangeWasLastFilenameValid(String filename, boolean expected) {
+		LogAnalyzer logAnalyzer = makeAnalyzer();
+		boolean validLogFilename = logAnalyzer.isValidLogFilename(filename);
+
+		Assert.assertEquals(expected, logAnalyzer.isLastFilenameValid());
 
 	}
 
